3ef68998250c90d231a5fec3a0bb91d69f35b321
paas <paas@paas-VirtualBox.(none)>
2014-07-24 08:58:18 +0530

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App1::Application.config.secret_key_base = '207be2f4b970610456302b91669231b853c8cd6a8f62e12bc8bd8d74f9fcef68f23c5e7dddb5549b93a50f74ead2fd60e61bc85653b5484ae324de2ae621e0b4'
